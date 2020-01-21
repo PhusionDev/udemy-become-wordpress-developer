8a99cc6c8f0c68958412d585f5723cbbcd88a065
jaredhodgkins <jared.hodgkins@gmail.com>
2020-01-21 10:35:30 -0500

    Lesson 40: Professors Post Type

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -21,26 +21,52 @@
     <div class="generic-content"><?php the_content(); ?></div>
 
     <?php
-          $today = date('Ymd');
-          $homepageEvents = new WP_Query(array(
-            'post_type' => 'event',
-            'meta_key' => 'event_date',
-            'orderby' => 'meta_value_num',
-            'order' => 'ASC',
-            'meta_query' => array(
-              array(
-                'key' => 'event_date',
-                'compare' => '>=',
-                'value' => $today,
-                'type' => 'numeric'
-              ),
-              array(
-                  'key' => 'related_programs',
-                  'compare' => 'LIKE',
-                  'value' => '"' . get_the_ID() . '"'
-              )
+    $relatedProfessors = new WP_Query(array(
+        'posts_per_page' => -1,
+        'post_type' => 'professor',
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
             )
-          ));
+        )
+    ));
+
+    if ($relatedProfessors->have_posts()) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+
+        while ($relatedProfessors->have_posts()) {
+            $relatedProfessors->the_post(); ?>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php }
+    }
+
+    wp_reset_postdata();
+
+    $today = date('Ymd');
+    $homepageEvents = new WP_Query(array(
+    'post_type' => 'event',
+    'meta_key' => 'event_date',
+    'orderby' => 'meta_value_num',
+    'order' => 'ASC',
+    'meta_query' => array(
+        array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+        ),
+        array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"'
+        )
+    )
+    ));
 
           if ($homepageEvents->have_posts()) {
             echo '<hr class="section-break">';
