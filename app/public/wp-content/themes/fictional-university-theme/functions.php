c747b0e07689145c761f317c31ee147c25ee14c9
jaredhodgkins <jared.hodgkins@gmail.com>
2020-01-27 18:02:18 -0500

    Lesson 48: Campus Post Type > This lesson was completed as much as possible without an api key for the google maps api. Replace $api['key'] with an actual api key within functions.php to work correctly.

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -78,3 +78,10 @@ function university_adjust_queries($query) {
     }
 }
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api) {
+    $api['key'] = 'REPLACE_WITH_GOOGLE_MAPS_API_KEY';
+    return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
