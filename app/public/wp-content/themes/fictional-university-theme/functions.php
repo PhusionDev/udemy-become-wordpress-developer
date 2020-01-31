6664e062475ec16fc697d7553f5690728465dd78
jaredhodgkins <jared.hodgkins@gmail.com>
2020-01-31 17:39:35 -0500

    Lesson 86: Note Permissions and Security (Part 2)

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -142,6 +142,11 @@ function ourLoginCSS() {
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data) {
+  if ($data['post_type'] == 'note') {
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+  }
+
   if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
     $data['post_status'] = "private";
   }
