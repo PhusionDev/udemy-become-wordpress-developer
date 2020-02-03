fd0d043270d2596bb513fb4a5811f7b973716fd1
jaredhodgkins <jared.hodgkins@gmail.com>
2020-02-03 02:45:36 -0500

    Lesson 102: Create a Custom Block Type (Part 1)

---
+++ app/public/wp-content/plugins/my-cool-border-box/my-block.js
@@ -0,0 +1,23 @@
+wp.blocks.registerBlockType('brad/border-box', {
+  title: 'My Cool Border Box',
+  icon: 'smiley',
+  category: 'common',
+  attributes: {
+    // any names
+    content: {type: 'string'},
+    color: {type: 'string'}
+  },
+  edit: function(props) {
+    return wp.element.createElement(
+      "div",
+      null,
+      wp.element.createElement(
+        "h3",
+        null,
+        "Your Cool Border Box"),
+      wp.element.createElement("input", { type: "text" }));
+  },
+  save: function(props) {
+    return null;
+  }
+})
