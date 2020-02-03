39bc315aeed727a93c5ea8179a3ab5c0e722afab
jaredhodgkins <jared.hodgkins@gmail.com>
2020-02-03 03:00:21 -0500

    Lesson 103: Create a Custom Block Type (Part 2)

--- app/public/wp-content/plugins/my-cool-border-box/my-block.js
+++ app/public/wp-content/plugins/my-cool-border-box/my-block.js
@@ -8,16 +8,28 @@ wp.blocks.registerBlockType('brad/border-box', {
     color: {type: 'string'}
   },
   edit: function(props) {
-    return wp.element.createElement(
-      "div",
-      null,
-      wp.element.createElement(
-        "h3",
-        null,
-        "Your Cool Border Box"),
-      wp.element.createElement("input", { type: "text" }));
+    function updateContent(event) {
+      props.setAttributes({content: event.target.value});
+    }
+
+    function updateColor(value) {
+      props.setAttributes({color: value.hex});
+    }
+
+    return React.createElement("div", null, React.createElement("h3", null, "Your Cool Border Box"), React.createElement("input", {
+      type: "text",
+      value: props.attributes.content,
+      onChange: updateContent
+    }), React.createElement(wp.components.ColorPicker, {
+      color: props.attributes.color,
+      onChangeComplete: updateColor
+    }));
   },
   save: function(props) {
-    return null;
+    return React.createElement("h3", {
+      style: {
+        border: "5px solid ".concat(props.attributes.color)
+      }
+    }, props.attributes.content);
   }
 })
